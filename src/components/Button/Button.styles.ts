e228599864c205b83bcdef954b2309483df40062
ocintnaf <ocintnaf@gmail.com>
2022-08-28 10:54:00 +0200

    change button color

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,8 +1,8 @@
-import styled from "styled-components";
-import { ButtonProps } from "./types";
+import styled from 'styled-components';
+import { ButtonProps } from './types';
 
 const StyledButton = styled.button<ButtonProps>`
-  color: red;
+  color: blue;
 `;
 
 export default StyledButton;
